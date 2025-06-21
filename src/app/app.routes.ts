60090ebc1eade55206cc60a93e6cdcdd2fec0e10
ahmed shalash <ashalash38@gmail.com>
2025-06-21 21:46:46 +0300

    upadate app  routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { KYCComponent } from './components/kyc/kyc.component';
 import { ResultsComponent } from './components/results/results.component';
+import { InvestmentAnalysisComponent } from './components/investment-analysis/investment-analysis.component';
+import { InvestmentResultsComponent } from './components/investment-results/investment-results.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'kyc', component: KYCComponent },
   { path: 'results', component: ResultsComponent },
+  { path: 'investment-analysis', component: InvestmentAnalysisComponent },
+  { path: 'investment-results', component: InvestmentResultsComponent },
   { path: '**', redirectTo: '' }
 ];
